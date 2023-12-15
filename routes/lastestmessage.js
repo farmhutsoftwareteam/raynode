ac2c0ab17fdf013ddf19e55fcbb01f38040070ab
Munyaradzi Makosa <munya@farmhutafrica.com>
2023-12-15 15:29:15 +0200

    get latest message

--- routes/lastestmessage.js
+++ routes/lastestmessage.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const User = require('../models/User'); // Assuming this is your user model
 
-// Route to get the last message of a thread
 router.get('/thread/:userId', async (req, res) => {
     try {
         const userId = req.params.userId;
@@ -19,14 +18,18 @@ router.get('/thread/:userId', async (req, res) => {
         const threadId = user.openaiThreadId;
         const messagesResponse = await openai.beta.threads.messages.list(threadId);
         const messages = messagesResponse.data;
-        
-        // Send the last message or the entire conversation
-        const lastMessage = messages[messages.length - 1];
-        res.send({ lastMessage });
+
+        // Filter for messages sent by the assistant and select the first one (latest message)
+        const assistantMessages = messages.filter(msg => msg.role === 'assistant');
+        const latestAssistantMessage = assistantMessages.length > 0 ? assistantMessages[0].content[0].text.value : "No messages from assistant found.";
+
+        res.send({ latestMessage: latestAssistantMessage });
     } catch (error) {
         console.error(`Error in getting thread: ${error}`);
         res.status(500).send({ error: 'Internal Server Error' });
     }
 });
+
+
 
 module.exports = router;
